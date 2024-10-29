ea824c77ab774a2348273b0918608dc8ff6e4b3d
Marcelle Novaes <137959998+marcellenovaes@users.noreply.github.com>
2024-10-29 16:28:26 -0300

    Add files via upload

---
+++ prova_listas.py
@@ -0,0 +1,9 @@
+# Crie uma lista contendo seis frutas de sua escolha.
+#  Depois de ter a lista pronta, converta essa lista em uma tupla.
+#  Por fim, exiba o conteúdo da tupla resultante para verificar as frutas que foram armazenadas.
+
+frutas = ['pera','uva', 'manga', 'morango', 'cereja', 'amora' ]
+
+salada_mista = tuple(frutas)
+
+print(salada_mista)
